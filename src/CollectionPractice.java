c5887871d8835093da593c1adbbc98c1740d248c
pankajkhatiwada <pankajkhatiwada@gmail.com>
2020-04-30 13:32:31 +0545

    Initial commit

---
+++ src/CollectionPractice.java
@@ -0,0 +1,11 @@
+/**
+ * @author Pankaj Khatiwada
+ */
+public class CollectionPractice {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------");
+        System.out.println("#### Collection ####");
+        System.out.println("---------------------");
+    }
+}
